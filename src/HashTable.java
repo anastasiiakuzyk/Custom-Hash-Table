9904ea273a080dd4ff81e1e1c26fc76bdc4752d8
Anastasiia <kuzykanastasija@gmail.com>
2021-05-17 22:07:06 +0300

    commit

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,38 +1,45 @@
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class HashTable <K, V> {
+public class HashTable<K, V> {
     private ArrayList<ArrayList<HashNode<K, V>>> bucketArray = new ArrayList<>();
-    //Constant for hashing
+
+    //constant for hashing
     private final double A = 0.56;
 
-    private int numBuckets = 100;
+    private int numBuckets = 100; //buckets can repeat
 
+    //amount of
     private int size = 0;
 
+    //memory allocation for nodes
     public HashTable() {
-        for (int i = 0; i < numBuckets; i++)
+        for (int i = 0; i < numBuckets; i++) {
             bucketArray.add(new ArrayList<>());
+        }
     }
 
     public int size() {
         return size;
     }
+
     public boolean isEmpty() {
         return size() == 0;
     }
 
-    private int getBucketIndex(K key) {
+    private int getHashCode(K key) {
         int hashCode = key.hashCode();
-        int index = (int)Math.floor((hashCode*A % numBuckets));
-        index = index < 0 ? index * -1 : index;
-        return index;
+        int index = (int) Math.floor((hashCode * A % numBuckets)); //closest index
+        if (index < 0) {
+            index = index * -1;
+        }
+        return index; //hash code
     }
 
     public V remove(K key) {
-        int bucketIndex = getBucketIndex(key);
+        int hashCode = getHashCode(key);
 
-        Iterator<HashNode<K, V>> it = bucketArray.get(bucketIndex).iterator();
+        Iterator<HashNode<K, V>> it = bucketArray.get(hashCode).iterator();
         while (it.hasNext()) {
             HashNode<K, V> y = it.next();
             if (y.key.equals(key)) {
@@ -46,39 +53,38 @@ public class HashTable <K, V> {
     }
 
     public V get(K key) {
-        int bucketIndex = getBucketIndex(key);
+        int hashCode = getHashCode(key);
 
-
-        for (HashNode<K, V> search : bucketArray.get(bucketIndex))
-            if (search.key.equals(key))
+        for (HashNode<K, V> search : bucketArray.get(hashCode)) {
+            if (search.key.equals(key)) {
                 return search.value;
+            }
+        }
 
         return null;
     }
 
     public void add(K key, V value) {
-        int bucketIndex = getBucketIndex(key);
-        ArrayList<HashNode<K, V>> possibleBucket = bucketArray.get(bucketIndex);
-        possibleBucket.add(new HashNode<K, V>(key, value));
+        int hashCode = getHashCode(key);
+        ArrayList<HashNode<K, V>> nodes = bucketArray.get(hashCode);
+        nodes.add(new HashNode<>(key, value));
         size++;
 
-
-        if ((1.0 * size) / numBuckets >= 0.8) {
-            ArrayList<ArrayList<HashNode<K, V>>> tmp = bucketArray;
+        //expanding table
+        if ((double) size / numBuckets >= 0.8) { // table is bigger than 80%
+            ArrayList<ArrayList<HashNode<K, V>>> temp = bucketArray;
             bucketArray = new ArrayList<>();
             numBuckets = 2 * numBuckets;
             size = 0;
-            for (int i = 0; i < numBuckets; i++)
+            for (int i = 0; i < numBuckets; i++) {
                 bucketArray.add(new ArrayList<>());
+            }
 
-            for (ArrayList<HashNode<K, V>> bucket : tmp) {
+            for (ArrayList<HashNode<K, V>> bucket : temp) {
                 for (HashNode<K, V> node : bucket) {
                     add(node.key, node.value);
                 }
             }
         }
-
     }
-
-
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+// Організувати хеш-таблицю, використовуючи хеш-функцію h (k) за методом множення для формування хеш-адреси, вирішення
+// колізій - методом  ланцюгів.
+// а) Написати  пошук і вставку за ключем.
+// б) Написати метод видалення по ключу.
+
 public class Main {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         HashTable<String, Integer> map = new HashTable<>();
         map.add("this", 1);
         map.add("coder", 2);
@@ -9,6 +13,7 @@ public class Main {
         map.add("hi", 5);
         System.out.println(map.get("this"));
         System.out.println(map.remove("coder"));
+        System.out.println(map.remove("this"));
         System.out.println(map.remove("this"));
     }
 }
